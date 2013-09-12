bfbee10ca838b390155e26a71cb8730942471ab1
karabom <komolema@gmail.com>
2013-09-12 22:23:30 +0200

    changes to license

---
+++ run.sh
@@ -0,0 +1,6 @@
+#This shell scripts will unpack the distribution tar, and run the container
+cd lesie-distribution/target
+tar -zxvf *.tar.gz
+cd /home/karabom/projects/lesieS/lesie-distribution/target/lesie-distribution-0.1-SNAPSHOT/bin
+export KARAF_DEBUG=true
+./karaf
